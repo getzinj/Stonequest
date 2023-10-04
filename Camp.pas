b7b0547eb5e4b6c13fc183fe786683a06a8af88d
Jeffrey Getzin <getzinj@gmail.com>
2023-10-03 20:25:22 -0400

    Fixing some errors in Camp.pas.

--- Camp.pas
+++ Camp.pas
@@ -11,7 +11,7 @@ Type
    ItemNode  = Record
                  Identified:   Boolean;      { Does anybody know what it is? }
                  Owner_Number: 1..6;         { Who owns the item? }
-                 Item_Number:  Integer;      { Which item is it? }
+                 Item_Num:     Integer;      { Which item is it? }
                  Next_Item:    ItemSet;      { Pointer to the rest of the items }
                End;
    Item_Pool = Array [Item_Type] of ItemSet; { List for each type of item }
@@ -91,7 +91,7 @@ Begin { Dead Characters }
    For Character:=1 to Party_Size do
      If (Alive(Member[Character])) then  { For every living member }
         Begin
-           Current_Party-Size:=Current_Party_Size+1;  { Increment survivor count }
+           Current_Party_Size:=Current_Party_Size+1;  { Increment survivor count }
         End;
 
    { With only six items, we might as well use a Bubblesort as any other sort.  This will put the dead characters at the rear of
@@ -233,11 +233,11 @@ Begin { Choose from List }
    Options:=[CHR(113)];  { So far, no options }
    Position:=0;
    TempPtr:=Choice_list;  { The temporary pointer points to beginning of list }
-   While (TmpPtr<>Nil) do  { While there are items on list }
+   While (TempPtr<>Nil) do  { While there are items on list }
       Begin { For each item }
          Item:=Item_List[TempPtr^.Item_Num];
          If (((Class in Item.Usable_By) or (Class1 in Item.Usable_By)) and  { if the item is usable }
-            (((Align=Item.Alignment) or (Item.Alignment=NoAlign))) then
+            (((Align=Item.Alignment) or (Item.Alignment=NoAlign)))) then
             Begin { Usable item }
                Position:=Position+1;                 { Increment the line counter }
                Print_Item_Node (TempPtr,Position);  { Print the choice }
@@ -259,7 +259,7 @@ Begin { Choose from List }
             Begin { For each item }
                Item:=Item_List[TempPtr^.Item_Num];  { Get the item }
                If (((Class in Item.Usable_By) or (Class1 in Item.Usable_By)) and  { If the item is usable }
-                  (((Align=Item.Alignment) or (Item.Alignment=NoAlign))) then
+                  (((Align=Item.Alignment) or (Item.Alignment=NoAlign)))) then
                      Begin
                         Position:=Position+1;  { Advance the counter }
                         Temp:=TempPtr;         { Get the current ptr. }
@@ -295,7 +295,7 @@ Var
 Begin { One Usable }
    Align:=Character.Alignment; Class:=Character.Class; Class1:=Character.PreviousClass;
    At_Least_One:=False;  Traveller:=Choices;
-   While ((Not At_LeastOne) and (Traveller<>Nil)) do
+   While ((Not At_Least_One) and (Traveller<>Nil)) do
       Begin { At Least One means that there is >=1 equippable item }
          Item:=Item_List[Traveller^.Item_Num];
          At_Least_One:=(Class in Item.Usable_By) or (Class1 in Item.Usable_By);
@@ -324,7 +324,7 @@ Begin { Select Item }
          SMG$Begin_Display_Update (CampDisplay);
          SMG$Erase_Display (CampDisplay);
          T:='[RETURN] for none';
-         SMG$Put_Chars (CampDisplay,1,23,39-(t.length div 2));
+         SMG$Put_Chars (CampDisplay,T,23,39-(t.length div 2));
          T:='Please select a '
              +Item_Name[Kind]
              +' for '
@@ -384,8 +384,8 @@ Begin { Not Stuck }
          Temp:=True;                  { So far, he can choose one }
          For Item:=1 to Character.No_Of_Items do  { For each item }
             Begin
-               Character_Item:=Item_List[Character.Item].Item_Num];
-               If (Character_Item.Kind=Kind) then  { If of the right type }}
+               Character_Item:=Item_List[Character.Item[Item].Item_Num];
+               If (Character_Item.Kind=Kind) then  { If of the right type }
                   Begin
                      Temp:=Temp AND Not(Character.Item[Item].Cursed);  { Cursed? }
                   End
