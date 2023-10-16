950997b303529c03d819270c2b552d7ba2e48097
Jeffrey Getzin <getzinj@gmail.com>
2023-10-15 22:59:27 -0400

    Fixed some typos.

--- Items.pas
+++ Items.pas
@@ -15,6 +15,7 @@ Var
    Spell: [External]Array [Spell_Name] of Varying [4] of Char;
    Item_Name: [External]Array [Item_Type] of Varying [7] of Char;
    AlignName: [External]Array [Align_Type] of Packed Array [1..7] of char;
+   AmountFile: [External]Number_File;
    Item_List: [External]List_of_Items;
 
 Value
@@ -108,19 +109,19 @@ Begin
 End;
 
 (******************************************************************************)
-
 Procedure HatesP (Var CantStand: Monster_Set);
 
 Var
    Pos: Integer;
    Loop: Monster_Type;
    Temp: Monster_Type;
+   MonsterNum: Integer;
    Answer: Char;
    T: Line;
 
-Begin
+Begin { HatesP }
    Repeat
-      Begin
+      Begin { Repeat }
          SMG$Begin_Display_Update (ScreenDisplay);
          SMG$Erase_Display (ScreenDisplay);
          SMG$Home_Cursor (ScreenDisplay);
@@ -130,18 +131,20 @@ Begin
          Pos:=1;
          T:='';
          For Loop:=Warrior to Enchanted do
-                 If Loop in CantStand then
-                    Begin
-                        T:=T+{ad(Monster_T[Loop],' ',20);
-                        If Pos/3<>Pos div 3 then
-                           T:=T+'    '
-                        Else
-                           Begin
-                              SMG$Put_Line (ScreenDisplay,T);
-                              T:='';
-                           End;
-                        Pos:=Pos+1;
-                    End;
+         Begin { For loop 1 }
+            If Loop in CantStand then
+            Begin
+                T:=T+{ad(Monster_T[Loop],' ',20);
+                If Pos/3<>Pos div 3 then
+                   T:=T+'    '
+                Else
+                   Begin
+                      SMG$Put_Line (ScreenDisplay,T);
+                      T:='';
+                   End;
+                Pos:=Pos+1;
+            End;
+         End; { For loop 1 }
          SMG$Put_Line (ScreenDisplay,T,0);
          SMG$Set_Cursor_ABS (ScreenDisplay,15,1);
          SMG$Put_Line (ScreenDisplay,
@@ -149,27 +152,27 @@ Begin
          Pos:=1;
          T:='';
          For Loop:=Warrior to Enchanted do
-             Begin
-                 T:=T
-                     +CHR(Ord(Loop)+65)
-                     +'  '
-                     +Pad(Monster_T[Loop],' ',20);
-                 If Pos/3<>pos div 3 then
-                     T:=T+'    '
-                 Else
-                     Begin
-                        SMG$Put_Line (ScreenDisplay, T);
-                        T:='';
-                     End;
-                 Pos:=Pos+1;
-             End;
+         Begin { For Loop 2 }
+             T:=T
+                 +CHR(Ord(Loop)+65)
+                 +'  '
+                 +Pad(Monster_T[Loop],' ',20);
+             If Pos/3<>pos div 3 then
+                 T:=T+'    '
+             Else
+                 Begin { third entry }
+                    SMG$Put_Line (ScreenDisplay, T);
+                    T:='';
+                 End;  { third entry }
+             Pos:=Pos+1;
+         End;  { For Loop 2 }
          SMG$Put_Line(ScreenDisplay,T,0);
          SMG$End_Display_Update (ScreenDisplay);
          Answer:=Make_Choice (
              ['A'..CHR(Ord(Enchanted)+65),
               ' ']);
          If Answer<>' ' then
-                 Begin
+                 Begin { Not space }
                     MonsterNum:=Ord(Answer)-65;
                     Temp:=Warrior;
                     While Ord(Temp)<>MonsterNum do
@@ -178,10 +181,10 @@ Begin
                        CantStand:=CantStand-[Temp]
                     Else
                        CantStand:=CantStand+[Temp];
-                 End;
-      End;
+                 End;  { Not space }
+      End;  { Repeat }
    Until Answer=' ';
-End;
+End;  { HatesP }
 
 
 (******************************************************************************)
@@ -192,6 +195,7 @@ Var
    Pos: Integer;
    Loop: Monster_Type;
    Temp: Monster_Type;
+   MonsterNum: Integer;
    Answer: Char;
    T: Line;
 
@@ -209,7 +213,7 @@ Begin
          For Loop:=Warrior to Enchanted do
                  If Loop in CantHurt then
                     Begin
-                        T:=T+{ad(Monster_T[Loop],' ',20);
+                        T:=T+Pad(Monster_T[Loop],' ',20);
                         If Pos/3<>Pos div 3 then
                            T:=T+'    '
                         Else
@@ -270,21 +274,21 @@ Begin
        +Cat[Position]
        +': ');
    Case Position of
-      1: SMG$Put_Chars (ScreenDisplay,String(Item, Item_Number));
-      2: SMG$Put_Chars (ScreenDisplay,Item, Item.Name);
-      3: SMG$Put_Chars (ScreenDisplay,Item, Item.True_Name);
-      4: SMG$Put_Chars (ScreenDisplay,Item, AlignName[Item.Alignment]);
-      5: SMG$Put_Chars (ScreenDisplay,Item, Item_Name[Item.Kind]);
+      1: SMG$Put_Chars (ScreenDisplay,String(Item.Item_Number));
+      2: SMG$Put_Chars (ScreenDisplay,Item.Name);
+      3: SMG$Put_Chars (ScreenDisplay,Item.True_Name);
+      4: SMG$Put_Chars (ScreenDisplay,AlignName[Item.Alignment]);
+      5: SMG$Put_Chars (ScreenDisplay,Item_Name[Item.Kind]);
       6: If Item.Cursed=True then
              SMG$Put_Chars (ScreenDisplay,
                  'Yes')
          Else
              SMG$Put_Chars (ScreenDisplay,
                  'No');
-      7: SMG$Put_Chars (ScreenDisplay,Item, String(Item.Special_Occurance_No));
-      8: SMG$Put_Chars (ScreenDisplay,Item, String(Item.Percentage_Breaks));
-      9: SMG$Put_Chars (ScreenDisplay,Item, Items[Item.Turns_Into].True_Name);
-      10: SMG$Put_Chars (ScreenDisplay,Item, String(Item.GP_Value));
+      7: SMG$Put_Chars (ScreenDisplay,String(Item.Special_Occurance_No));
+      8: SMG$Put_Chars (ScreenDisplay,String(Item.Percentage_Breaks));
+      9: SMG$Put_Chars (ScreenDisplay,Items[Item.Turns_Into].True_Name);
+      10: SMG$Put_Chars (ScreenDisplay,String(Item.GP_Value));
       11: If AmountFile^=0 then
              SMG$Put_Chars (ScreenDisplay,
                  'None')
@@ -513,7 +517,7 @@ Begin
              'F)orward, B)ack, E)xit',0);
          SMG$End_Display_Update (ScreenDisplay);
          Answer:=Make_Choice (['F','B','E']);
-         If (Answer='F') and (First+Page+size<=Table_Size) then
+         If (Answer='F') and (First+Page_size<=Table_Size) then
             Begin
                First:=First+Page_Size;
                Last:=First+Page_Size;
@@ -540,6 +544,7 @@ Var
   T: Line;
   Answer: Char;
   Old_Slot,New_Slot: Integer;
+  Old_Num,New_Num: Integer;
   Temp_Record: Item_Record;
 
 Begin
@@ -583,7 +588,7 @@ Begin
          Close(AmountFile);
 
          Temp_Record:=Item_List[Old_Slot];
-         Item_List[Old_Slot]:=Item_Lit[New_Slot];
+         Item_List[Old_Slot]:=Item_List[New_Slot];
          ItemList[New_Slot]:=Temp_Record;
 
          Item_List[New_Slot].Item_Number:=New_Slot;
