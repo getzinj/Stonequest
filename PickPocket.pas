f781368a327395834e0d32ee20e647c424069345
Jeffrey Getzin <getzinj@gmail.com>
2023-11-01 21:58:59 -0400

    More pickpocket.

--- PickPocket.pas
+++ PickPocket.pas
@@ -322,6 +322,287 @@ End;
 
 (******************************************************************************)
 
+Function Get_Sex (Job: Integer): [Volatile]Integer;
+
+Begin
+  Case Job of
+     Brawler,Bully,CommonThief,Magus,Drunk,Militia,Soldier: Get_Sex:=Min(Roll_Die(FemaleS),Roll_Die(FemaleS));
+     Prostitute: Get_Sex:=FemaleS;
+     Clerk,Gentleman,Jester,Nobleman,Official,Pimp: Get_Sex:=MaleS;
+     Cleric_C,Beggar,Gambler,Royalty: Get_Sex:=Roll_Die(FemaleS);
+     Otherwise Get_Sex:=Roll_Die (FemaleS);
+  End;
+End;
+
+(******************************************************************************)
+
+Function Get_Race (Job: Integer): [Volatile]Integer;
+
+Var
+  Temp1,Temp2: Integer;
+
+Begin
+   Temp1:=Roll_Die(NumenoreanR);
+   Temp2:=Roll_Die(NumenoreanR);
+   Temp1:=Min(Temp1,Temp2);
+
+   Case Job of
+      Beggar,Drunk,Gambler,Brawler,Prostitute,Pimp,Cleric_C,Soldier,Militia,Magus: ;
+      Clerk,Jester: If Temp1 in [HfOrcR,DwarfR,HfOgreR,LizardManR,CentaurR,DrowR,NumenoreanR] then Temp1:=HumanR;
+      CommonThief,Bully: If Temp1=NumenoreanR then Temp1:=HumanR;
+      Gentleman,Lady,Nobleman,Official,Royalty: Temp1:=HumanR;
+      Otherwise Temp1:=HumanR;
+   End;
+   Get_Race:=Temp1;
+End;
+
+(******************************************************************************)
+
+Procedure Get_Physical (Var Temp: Description_Record; Sex,Race,Job: Integer);
+
+Var
+  B1,H1,W1: Integer;
+
+Begin
+  B1:=0;  H1:=0;  W1:=0;
+  B1:=Roll_Die(6)+Roll_Die(6)+Roll_Die(6);
+  H1:=Roll_Die(6)+Roll_Die(6)+Roll_Die(6);
+  W1:=Roll_Die(6)+Roll_Die(6)+Roll_Die(6);
+
+  Case Race of
+     HumanR,HfElfR:  ;
+     HfOrcR:          Begin  B1:=B1+2;  H1:=H1-1;   W1:=W1+2;   End;
+     DwarfR:          Begin  B1:=B1+1;  H1:=H1-8;   W1:=W1-4;   End;
+     ElvenR,DrowR:    Begin  B1:=B1-1;  H1:=H1-2;   W1:=W1-2;   End;
+     HfOgreR:         Begin  B1:=B1+4;  H1:=H1+4;   W1:=W1+8;   End;
+     GnomeR,HobbitR:  Begin  B1:=B1-2;  H1:=H1-8;   W1:=W1-6;   End;
+     LizardManR:      Begin  B1:=B1+2;  H1:=H1+1;   W1:=W1+1;   End;
+     CentaurR:        Begin  B1:=B1+1;  H1:=H1-1;   W1:=W1+4;   End;
+     QuicklingR:      Begin  B1:=B1+6;  H1:=H1-12;  W1:=W1-12;  End;
+     NumenoreanR:     Begin  W1:=W1-2;  End;
+     Otherwise ;
+  End;
+
+  Case Sex of
+     MaleS:  Begin  B1:=B1+1;    H1:=H1+1;  W1:=W1+1;  End;
+     FemaleS:  Begin  B1:=B1-1;  H1:=H1-1;  W1:=W1-1;  End;
+  End;
+
+  Case Job of
+     Beggar,Drunk,Prostitute,CommonThief,Bully,Gambler,Gentleman,Lady,Official,Magus: ;
+     Jester: Begin  B1:=B1+1;  W1:=8;  End;
+     Brawler,Cleric_c,Soldier,Militia: B1:=B1+2;
+     Clerk:  B1:=B1-1;
+     Nobleman,Royalty: W1:=W1+3;
+     Otherwise ;
+  End;
+
+  Temp[Build]:=B1;
+  Temp[Height]:=H1;
+  Temp[Weight]:=W1;
+End;
+
+(******************************************************************************)
+
+Function Get_Money (Job: Integer): [Volatile]Integer;
+
+Begin
+   Case Job of
+      Beggar: Get_Money:=2-Roll_Die(2);
+      Jester: Get_Money:=Roll_Die(3)*Roll_Die(6);
+      Pimp,Prostitute: Get_Money:=(Roll_Die(8)*Roll_Die(100))-1;
+      CommonThief: Get_Money:=2*Roll_Die(20)-2;
+      Bully,Brawler: Get_Money:=Roll_Die(3)*Roll_Die(20);
+      Clerk: Get_Money:=3 * Roll_Die(7);
+      Soldier,Militia: Get_Money:=Roll_Die(20)+Roll_Die(20);
+      CLeric_C,Gentleman,Lady,Official,Gambler: Get_Money:=Max(Roll_Die(20),Roll_Die(100));
+      Nobleman,magus: Get_Money:=(Roll_Die(4)*Roll_Die(1000))+Roll_Die(100);
+      Royalty: Get_Money:=Roll_Die(12000);
+      Otherwise Get_Money:=0;
+   End;
+End;
+
+(******************************************************************************)
+
+Procedure Get_Equiptment (Var Temp: Description_Record;  Job: Integer);
+
+Begin
+   Case Job of
+       Beggar:                   Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=3;   End;
+       Jester:                   Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=9;   End;
+       Drunk:                    Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=3;   End;
+       Prostitute:               Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=3+Roll_Die(3);   End;
+       Pimp:                     Begin  Temp[Armors]:=0;   Temp[Weapons]:=3;  Temp[Clothes]:=21;   End;
+       CommonThief:              Begin  Temp[Armors]:=3;   Temp[Weapons]:=3;  Temp[Clothes]:=8;   End;
+       Bully:                    Begin  Temp[Armors]:=(Roll_Die(2)-1)*(Roll_Die(6)*3);
+                                        Temp[Weapons]:=(Roll_Die(2)-1)*(Roll_Die(6)*3);
+                                        Temp[Clothes]:=9;
+                                 End;
+       Gambler:                  Begin  Temp[Armors]:=0;   Temp[Weapons]:=3;  Temp[Clothes]:=7+Roll_Die(Temp[Money] div 10);   End;
+       Brawler:                  Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=9;   End;
+       Clerk:                    Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=8;   End;
+       Cleric_c,Soldier,Militia: Begin  Temp[Armors]:=16;  Temp[Weapons]:=16; Temp[Clothes]:=5+Roll_Die(9);   End;
+       Gentleman,Lady,Official:  Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=16;   End;
+       Nobleman,Royalty:         Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=18;   End;
+       Magus:                    Begin  Temp[Armors]:=0;   Temp[Weapons]:=3;  Temp[Clothes]:=15;   End;
+       Otherwise                 Begin  Temp[Armors]:=0;   Temp[Weapons]:=0;  Temp[Clothes]:=0;   End;
+   End;
+End;
+
+(******************************************************************************)
+
+Function Get_Distraction (Job: Integer): [Volatile]Integer;
+
+Var
+  D1: Integer;
+
+Begin
+   D1:=Roll_Die(6)+Roll_Die(6)+Roll_Die(6);
+
+   Case Job of
+     Beggar: D1:=D1+3;
+     Jester: D1:=3;
+     Drunk: D1:=D1+12;
+     Prostitute: D1:=3;
+     Pimp: D1:=D1-1;
+     CommonThief: D1:=-12;
+     Bully,Brawler,Cleric_C: D1:=D1+2;
+     Clerk,Soldier,Militia: ;
+     Gambler: D1:=D1+1;
+     Gentleman,Lady: D1:=D1+3;
+     Nobleman: D1:=D1+1;
+     Official: D1:=D1+2;
+     Royalty,Magus: D1:=D1+10;
+     Otherwise ;
+   End;
+   Get_Distraction:=D1;
+End;
+
+
+(******************************************************************************)
+
+Function Get_Detect (Temp: Description_Record): [Volatile]Integer;
+
+{ Chance the person will see the character. This reduces, but does not eliminate the chance of picking pockets }
+
+Var
+  D1: Integer;
+
+Begin
+   Case Temp[Job] of
+     Beggar: D1:=99;
+     Jester: D1:=80;
+     Drunk: D1:=40;
+     Prostitute: D1:=80;
+     Pimp: D1:=80;
+     CommonThief: D1:=90;
+     Bully: D1:=50;
+     Gambler,Cleric_c: D1:=20;
+     Brawler: D1:=50;
+     Clerk: D1:=10;
+     Soldier,Militia: D1:=85;
+     Gentleman,Lady: D1:=80;
+     Nobleman: D1:=90;
+     Official: D1:=70;
+     Royalty: D1:=99;
+     Magus: D1:=75;
+     Otherwise D1:=65;
+   End;
+   D1:=D1-(Temp[Distraction]*2);
+   Get_Detect:=D1;
+End;
+
+(******************************************************************************)
+
+Function Get_Townsperson: Description_Record;
+
+Var
+  Temp: Description_Record;
+
+Begin
+   Temp:=Zero;
+   Temp[EyeColor]:=Get_Eyecolor;
+   Temp[HairColor]:=Get_Haircolor;
+   Temp[Job]:=Get_Job;
+   Temp[Sex]:=Get_Sex (Temp[Job]);
+   Temp[Race]:=Get_Race (Temp[Job]);
+
+   Get_Physical (Temp,Temp[Sex],Temp[Race],Temp[Job]);
+
+   Temp[Money]:=Get_Money (Temp[Job]);
+
+   Get_Equiptment (Temp,Temp[Job]);
+
+   Temp[Distraction]:=Get_Distraction (Temp[Job]);
+   Temp[DetectChance]:=Get_Detect (Temp);
+
+  Get_Townsperson:=Temp;
+End;
+
+(******************************************************************************)
+
+Function Class_Chance (Class: Class_Type; Level: Integer): Integer;
+
+Var
+   Chance: Integer;
+
+Begin
+   Case Class of
+      Thief,Ninja:          Chance:=20+(5*Level);
+      Antipaladin,Assassin: Chance:=5+(5*Level);
+      Paladin:              Chance:=1;
+      Bard:                 Chance:=5*Level;
+      NoClass:              Chance:=0;
+      Otherwise             Chance:=Level;
+   End;
+   Class_Chance:=Chance;
+End;
+
+(******************************************************************************)
+
+Function Pick_Pocket_Chance (Character: Character_Type; Person: Description_Record; Noticed: Boolean): Integer;
+
+Var
+   Chance: Integer;
+   Class1,Class2: Class_Type;
+   Level1,Level2: Integer;
+
+Begin
+   Class1:=Character.Class;            Level1:=Character.Level;
+   Class2:=Character.PreviousClass;    Level2:=Character.Previous_Lvl;
+   Chance:=Max(Class_Chance(Class1,Level1),Class_Chance(Class2,Level2));
+
+   Case Character.Race of
+       Dwarven:   Chance:=Chance+15;
+       Gnome:     Chance:=Chance+10;
+       Hobbit,HfOrc: Chance:=Chance+5;
+       HfOgre:    Chance:=Chance-10;
+       Quickling,Drow,Elven:  Chance:=Chance+20;
+   End;
+
+   Case Character.Abilities[4] of
+       3..8: Chance:=Chance-20;
+       9,10: Chance:=Chance-10;
+       11: Chance:=Chance-5;
+       18: Chance:=Chance+5;
+       19,20: Chance:=Chance+10;
+       21,22: Chance:=Chance+15;
+       23,24: Chance:=Chance+20;
+       25: Chance:=Chance+25;
+   End;
+
+   Chance:=Chance-Round(1.25 * Person[Job]);
+   Chance:=Chance-Person[DetectChance];
+   Chance:=Chance+(2*(Character.Abilities[7]-9));  { add luck into it }
+   If Noticed then Chance:=Chance-80
+   Else Chance:=Chance+(2*Person[Distraction]);
+
+   If Chance>96 then Chance:=95;
+   Pick_Pocket_Chance:=Chance;
+End;
+
+(******************************************************************************)
+
 { TODO: Enter this code }
 
 [Global]Procedure Pick_Pockets (Var Party: Party_Type;  Var Party_Size: Integer);
